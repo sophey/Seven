27ad1aa316c9c2cc0b2ef278c2f853f9c28fc263
Hyeji Kim <hj951008@gmail.com>
2017-01-13 14:27:05 -0500

    cardsho

--- app/src/main/java/com/example/demouser/seven/GameActivity.java
+++ app/src/main/java/com/example/demouser/seven/GameActivity.java
@@ -5,8 +5,56 @@ import android.os.Bundle;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class GameActivity extends AppCompatActivity {
+    private ArrayList<String> player1Deck;
+    private ArrayList<String> player2Deck;
+    private String currentCard;
+    private Map<String,Integer> deck;
+    private boolean p1Turn;
+
+    private boolean canPutCard( String cardChosen){
+        String[] currentParts = currentCard.split("-");
+        String[] chosenParts = cardChosen.split("-");
+        String currentCardColor = currentParts[0];
+        String currentCardNum = currentParts[1];
+        String chosenColor = chosenParts[0];
+        String chosenNum = chosenParts[1];
+        // compare the current card to the card chosen
+        if(currentCardColor.equals(chosenColor)){
+            if(chosenNum.equals("skip") || chosenNum.equals("reverse")){
+                // same turn
+                // return true
+                return true;
+            }
+
+            else if(chosenNum.equals("draw2")){
+
+            }
+            else{
+                changeTurn();
+                return true;
+            }
+
+        }
+        else if(cardChosen.equals("wild")){
+            // give the user a window to choose color
+        }
+        else{
+
+        }
+
+    }
+
+    private void changeTurn(){
+        if(p1Turn){
+            p1Turn = false;
+        }
+        else{
+            p1Turn = true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +63,6 @@ public class GameActivity extends AppCompatActivity {
 
 //        ArrayList<String> player1Cards =
 //
-//        for ()
+//        for `()
     }
 }
