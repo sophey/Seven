220b05bfa9d3a166ed22f5a4597ab5c173524c12
sophey <dong22s@mtholyoke.edu>
2017-01-19 18:17:57 -0500

    did stuff like multiplayer and win conditions and wild card edits and sign in/out stuff yo

---
+++ app/src/main/java/com/example/demouser/seven/WinActivity.java
@@ -0,0 +1,15 @@
+package com.example.demouser.seven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win);
+    }
+}
